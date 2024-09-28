176d52db42c92ec1bc0335c889aa76f624c5172a
abhixv <abhi.sharma1@hotmail.com>
2024-09-28 21:25:23 +0530

    Spacewar: Keep building fingerprint HIDL interface lib
    Signed-off-by: abhixv <abhi.sharma1@hotmail.com>

--- device.mk
+++ device.mk
@@ -268,6 +268,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint-service.nothing
 
 # Gatekeeper
